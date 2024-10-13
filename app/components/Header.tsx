6eb3fdb9f0d56249e36239cabaed07370d69996e
medericc <bleublanctour1@>
2024-10-13 04:32:08 +0200

    e

--- app/components/Header.tsx
+++ app/components/Header.tsx
@@ -9,26 +9,61 @@ const Header = () => {
       transition={{ duration: 1 }}
       className="relative text-center mt-10 md:mt-20"
     >
-      <div className="relative inline-block">
-        {/* Title with overlap on the flag */}
+      <motion.img
+        src="/arrow.png"
+        alt="Arrow Background"
+        initial={{ opacity: 0 }}
+        animate={{ opacity: 0.5 }}
+        transition={{ duration: 2 }}
+        className="absolute top-0 left-1/2 transform -translate-x-1/2 w-[300vw] md:w-[30vw] z-50"
+      />
+      <div className="relative inline-block mt-[30vw]">
+
+        {/* Numéro doré au-dessus du nom */}
+        <motion.h2
+  initial={{ scale: 0 }}
+  animate={{ scale: 1 }}
+  transition={{ duration: 1.5 }}
+  className="font-bold absolute top-[-12vw] md:top-[-8vw] left-[30%] transform -translate-x-1/2 z-20 uppercase"
+  style={{
+    background: 'linear-gradient(90deg, #eec99e, #ffeec2)',
+    WebkitBackgroundClip: 'text',
+    WebkitTextFillColor: 'transparent', // Safari/Chrome support
+    backgroundClip: 'text', // Pour les autres navigateurs
+    fontFamily: 'Heroic, Helvetica, Arial, sans-serif',
+    fontSize: '2rem',
+    fontWeight: 700,
+    letterSpacing: '0',
+    lineHeight: 0.9,
+    paddingTop: '0.1em',
+    textTransform: 'uppercase', 
+  }}
+>
+  NUMBER #0
+</motion.h2>
+
+
+        {/* Titre CARLA */}
         <motion.h1
           initial={{ scale: 0 }}
           animate={{ scale: 1 }}
           transition={{ duration: 1.5 }}
-          className="text-[18vw] md:text-[10vw] font-bold absolute top-[-8vw] md:top-[-5vw] left-1/4 transform -translate-x-1/2 text-white uppercase z-10"
+          className="text-[18vw] md:text-[10vw] font-bold absolute top-[-8vw] md:top-[-5vw] left-[20%] transform -translate-x-1/2 text-white uppercase z-10 mt-[7vw]"
         >
           CARLA
         </motion.h1>
+
+        {/* Titre LEITE */}
         <motion.h1
           initial={{ scale: 0 }}
           animate={{ scale: 1 }}
           transition={{ duration: 1.5 }}
-          className="text-[18vw] md:text-[10vw] font-bold absolute top-[5vw] md:top-[2vw] left-1/4 transform -translate-x-1/2 text-white uppercase z-10"
+          className="text-[18vw] md:text-[10vw] font-bold absolute top-[5vw] md:top-[2vw] left-[26%] transform -translate-x-1/2 text-white uppercase z-10 mt-[12vw]"
         >
           LEITE
         </motion.h1>
 
-        {/* Video Flag */}
+        {/* Vidéo du drapeau */}
         <motion.div
           initial={{ scale: 0 }}
           animate={{ scale: 1 }}
@@ -41,25 +76,27 @@ const Header = () => {
             playsInline
             autoPlay
             preload="auto"
-            className="w-full h-full object-cover opacity-60"
+            className="mt-[6.75rem] w-full h-full object-cover opacity-60"
           >
             <source src="/fr.mp4" type="video/mp4" />
           </video>
         </motion.div>
 
-        {/* Character */}
+        {/* Personnage */}
         <motion.div
           initial={{ opacity: 0, x: 100 }}
           animate={{ opacity: 1, x: 0 }}
           transition={{ delay: 2, duration: 1 }}
-          className="absolute bottom-[-10px] left-1/4 transform -translate-x-1/2 z-20"
+          className="absolute bottom-[-40px] left-[-7%] transform -translate-x-1/2 z-20"
         >
           <img
             src="/perso.png"
             alt="Personnage"
-            className="w-[50vw] md:w-[20vw] h-auto"
+            className="w-[116vw] md:w-[20vw] h-auto"
+            style={{ maxWidth: 'none' }}
           />
         </motion.div>
+
       </div>
 
       {/* Bande noire entre le personnage et le drapeau */}
@@ -67,49 +104,40 @@ const Header = () => {
         initial={{ opacity: 0 }}
         animate={{ opacity: 1 }}
         transition={{ duration: 1.5 }}
-        className="absolute top-[25vh] md:top-[30vh] left-0 w-full h-[20vh] bg-gradient-to-t from-[#1a1a1a] via-transparent z-5"
+        className="absolute md:top-[30vh] left-0 w-full h-[20vh] bg-gradient-to-t from-[#1a1a1a] via-transparent z-5"
       />
 
-       {/* Stats Section */}
-       <motion.div
+      {/* Section des stats */}
+      <motion.div
         initial={{ opacity: 0, x: 50 }}
         animate={{ opacity: 1, x: 0 }}
         transition={{ duration: 1.5 }}
-        className="flex justify-center mt-12 md:mt-24 gap-5 md:gap-10 text-uppercase tracking-wide text-white "
+        className="flex justify-center mt-12 md:mt-24 gap-5 md:gap-10 text-uppercase tracking-wide text-white"
       >
         <ul
-          className="flex w-full md:w-auto justify-around md:gap-10  md:px-0 bg-black pt-4 pb-4 ml-2 mr-2 "
+          className="flex w-full md:w-auto justify-around md:gap-10 md:px-0 bg-black pt-4 pb-4 ml-2 mr-2"
           data-v-eb447b1e
         >
-          <li
-            className="text-center border-r md:border-r-0 md:border-b-0 border-gray-600 flex-1 md:w-auto "
-            data-v-eb447b1e
-          >
+          <li className="text-center border-r md:border-r-0 md:border-b-0 border-gray-600 flex-1 md:w-auto" data-v-eb447b1e>
             <p className="text-sm md:text-base mb-0">Wins</p>
             <p className="text-4xl md:text-6xl font-bold">30</p>
           </li>
-          <li
-            className="text-center border-r md:border-r-0 md:border-b-0 border-gray-600 flex-1 md:w-auto "
-            data-v-eb447b1e
-          >
+          <li className="text-center border-r md:border-r-0 md:border-b-0 border-gray-600 flex-1 md:w-auto" data-v-eb447b1e>
             <p className="text-sm md:text-base mb-0">Losses</p>
             <p className="text-4xl md:text-6xl font-bold">05</p>
           </li>
-          <li
-            className="text-center border-r md:border-r-0 md:border-b-0 border-gray-600 flex-1 md:w-auto "
-            data-v-eb447b1e
-          >
-            <p className="text-sm md:text-base mb-0 pb-1 ">Draws</p>
+          <li className="text-center border-r md:border-r-0 md:border-b-0 border-gray-600 flex-1 md:w-auto" data-v-eb447b1e>
+            <p className="text-sm md:text-base mb-0 pb-1">Draws</p>
             <p className="text-4xl md:text-6xl font-bold">00</p>
           </li>
-          <li className="text-center flex-1 md:w-auto " data-v-eb447b1e>
+          <li className="text-center flex-1 md:w-auto" data-v-eb447b1e>
             <p className="text-sm md:text-base mb-0">KO</p>
             <p className="text-4xl md:text-6xl font-bold">18</p>
           </li>
         </ul>
       </motion.div>
 
-      {/* Horizontal Line */}
+      {/* Ligne horizontale */}
       <div className="h-[3px] w-4/5 bg-white mx-auto mt-5" />
     </motion.header>
   );
