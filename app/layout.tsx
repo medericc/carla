17a520f4d03dbd98a313d0b2a2b91433c90fbfa8
medericc <bleublanctour1@>
2024-10-18 17:55:44 +0200

    e

--- app/layout.tsx
+++ app/layout.tsx
@@ -25,8 +25,7 @@ export default function RootLayout({ children }: { children: React.ReactNode }) 
   return (
     <html lang="fr">
       <Head>
-      <meta name="viewport" content="width=device-width, initial-scale=1.0, user-scalable=no">
-
+        <meta name="viewport" content="width=device-width, initial-scale=1.0, user-scalable=no" />
         <title>{metadata.title}</title>
         <meta name="description" content={metadata.description} />
       </Head>
