fcd3f97202191be0986213ee6125129a722e5bc2
medericc <bleublanctour1@>
2024-11-01 19:00:59 +0100

    second

--- app/directory/page.tsx
+++ app/directory/page.tsx
@@ -29,9 +29,10 @@ export default function Directory() {
   const [filteredData, setFilteredData] = useState<MatchData[]>([]);
   const [numResults, setNumResults] = useState(1);
   const [sortOrder, setSortOrder] = useState<"top" | "bottom">("top");
-
+  const [competitionFilter, setCompetitionFilter] = useState<string>("");
   // State pour stocker les totaux de PTS, AST, et RBD
   const [totals, setTotals] = useState({ PTS: 0, AST: 0, RBD: 0 });
+  const [showCompetitionFilter, setShowCompetitionFilter] = useState(false);
 
   useEffect(() => {
     Papa.parse("/carla.csv", {
@@ -50,16 +51,42 @@ export default function Directory() {
     });
   }, []);
 
+
+  const toggleCompetitionFilter = () => {
+    setShowCompetitionFilter(!showCompetitionFilter);
+  };
+  
+  const filterByCompetition = (data: MatchData[]) => {
+    switch (competitionFilter) {
+      case "Championnat":
+        return data.filter((match) => match.CHAMPIONNAT === "LFB");
+      case "PlayOff":
+        return data.filter(
+          (match) => match.CHAMPIONNAT.includes("PO") || match.CHAMPIONNAT.includes("CDF")
+        );
+      case "National Team":
+        return data.filter((match) => match.CHAMPIONNAT.includes("EURO"));
+      case "Clubs":
+        return data.filter((match) => !match.CHAMPIONNAT.includes("EURO"));
+      default:
+        return data;
+    }
+  };
   const handleFilter = () => {
     if (data.length === 0) return;
-    const sortedData = [...data].sort((a, b) => {
+  
+    const competitionFilteredData = filterByCompetition(data);
+  
+    const sortedData = [...competitionFilteredData].sort((a, b) => {
       const aValue = a[stat] as number;
       const bValue = b[stat] as number;
       return sortOrder === "top" ? bValue - aValue : aValue - bValue;
     });
+  
     setFilteredData(sortedData.slice(0, numResults));
   };
-
+  
+  
   return (
     <div className="p-4 md:p-6">
     
@@ -173,7 +200,32 @@ export default function Directory() {
             <option value="bottom">Pires</option>
           </select>
         </div>
-      </div>
+        <button
+    onClick={toggleCompetitionFilter}
+    className="w-full sm:w-auto bg-blue-600 text-zinc-200 font-bold p-2 rounded mt-2 sm:col-span-3 mx-auto"
+  >
+    {showCompetitionFilter ? "MASK" : "SHOW"}
+  </button>
+
+  {showCompetitionFilter && (
+    <div className="flex flex-col mt-2">
+      <label htmlFor="competition-filter" className="mb-1 text-sm">Compétition :</label>
+      <select
+        id="competition-filter"
+        value={competitionFilter}
+        onChange={(e) => setCompetitionFilter(e.target.value)}
+        className="border p-2 text-black rounded"
+      >
+        <option value="">Sélectionner</option>
+        <option value="Championnat">Championnat</option>
+        <option value="PlayOff">PlayOff</option>
+        <option value="National Team">National Team</option>
+        <option value="Clubs">Clubs</option>
+      </select>
+    </div>
+  )}
+</div>
+      
 
       <button
     onClick={handleFilter}
