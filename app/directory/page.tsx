7755abe8e2d2702c5745411e6bf8c86646fa6a32
medericc <bleublanctour1@>
2024-11-01 06:23:21 +0100

    second

--- app/directory/page.tsx
+++ app/directory/page.tsx
@@ -1,4 +1,3 @@
-// app/directory/page.tsx
 "use client";
 import { useState, useEffect } from "react";
 import Papa from "papaparse";
@@ -31,6 +30,9 @@ export default function Directory() {
   const [numResults, setNumResults] = useState(1);
   const [sortOrder, setSortOrder] = useState<"top" | "bottom">("top");
 
+  // State pour stocker les totaux de PTS, AST, et RBD
+  const [totals, setTotals] = useState({ PTS: 0, AST: 0, RBD: 0 });
+
   useEffect(() => {
     Papa.parse("/carla.csv", {
       header: true,
@@ -38,107 +40,160 @@ export default function Directory() {
       complete: (result) => {
         const parsedData = result.data as MatchData[];
         setData(parsedData);
+
+        // Calcul des totaux
+        const ptsTotal = parsedData.reduce((sum, match) => sum + match.PTS, 0);
+        const astTotal = parsedData.reduce((sum, match) => sum + match.AST, 0);
+        const rbdTotal = parsedData.reduce((sum, match) => sum + match.RBD, 0);
+        setTotals({ PTS: ptsTotal, AST: astTotal, RBD: rbdTotal });
       },
     });
   }, []);
 
-  // Fonction pour filtrer et trier les données
   const handleFilter = () => {
     if (data.length === 0) return;
-
-    // Trier les données selon la statistique et l'ordre choisi
     const sortedData = [...data].sort((a, b) => {
       const aValue = a[stat] as number;
       const bValue = b[stat] as number;
       return sortOrder === "top" ? bValue - aValue : aValue - bValue;
     });
-
-    // Limiter les résultats selon le nombre de résultats souhaité
     setFilteredData(sortedData.slice(0, numResults));
   };
 
   return (
     <div className="p-4 md:p-6">
-    <h1 className="text-3xl font-bold mb-4 text-center">StatsCenter</h1>
+    
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+<ul className="flex w-full md:w-full justify-between p-4 bg-red-800 rounded-lg mt-4 ml-4 mr-4 max-w-[90%] shadow-md">
+  <li className="text-center flex-1 text-white border-r border-red-700 last:border-0">
+    <p className="text-xs font-semibold uppercase opacity-70 mb-1">PTS</p>
+    <p className="text-2xl font-bold tracking-wide">{data.reduce((total, match) => total + Number(match.PTS), 0)}</p>
+  </li>
   
-    {/* Options de sélection et filtre */}
-    <div className="mb-4 grid grid-cols-1 gap-4 sm:grid-cols-3 sm:gap-2 items-center">
-      {/* Statistique sélectionnée */}
-      <div className="flex flex-col">
-        <label htmlFor="stat-select" className="mb-1 text-sm">Sélectionnez une statistique :</label>
-        <select
-          id="stat-select"
-          value={stat}
-          onChange={(e) => setStat(e.target.value as keyof MatchData)}
-          className="border p-2 text-black rounded"
-        >
-          <option value="PTS">Points</option>
-          <option value="RBD">Rebonds</option>
-          <option value="AST">Assists</option>
-          <option value="STL">Steals</option>
-          <option value="BLK">Blocks</option>
-          <option value="EFF">Evaluation</option>
-          <option value="3P">3 Points</option>
-          <option value="1P">Lancer-Franc</option>
-          <option value="P">Paniers</option>
-          <option value="TO">Balles Perdues</option>
-        </select>
-      </div>
+  <li className="text-center flex-1 text-white border-r border-red-700 last:border-0">
+    <p className="text-xs font-semibold uppercase opacity-70 mb-1">AST</p>
+    <p className="text-2xl font-bold tracking-wide">{data.reduce((total, match) => total + Number(match.AST), 0)}</p>
+  </li>
   
-      {/* Nombre de résultats */}
-      <div className="flex flex-col">
-        <label htmlFor="num-results" className="mb-1 text-sm">Top :</label>
-        <input
-          id="num-results"
-          type="number"
-          value={numResults}
-          onChange={(e) => setNumResults(Number(e.target.value))}
-          className="border p-2 text-black rounded w-full"
-          min={1}
-        />
-      </div>
-  
-      {/* Ordre de tri */}
-      <div className="flex flex-col">
-        <label htmlFor="sort-order" className="mb-1 text-sm">Rang :</label>
-        <select
-          id="sort-order"
-          value={sortOrder}
-          onChange={(e) => setSortOrder(e.target.value as "top" | "bottom")}
-          className="border p-2 text-black rounded"
-        >
-          <option value="top">Meilleurs</option>
-          <option value="bottom">Pires</option>
-        </select>
-      </div>
-    </div>
-  
-    <button
-      onClick={handleFilter}
-      className="w-full sm:w-auto bg-red-800 text-white p-2 rounded mt-2 sm:col-span-3"
-    >
-      Filtrer
-    </button>
-  
-    {/* Affichage des cartes de match */}
-    <div className="grid grid-cols-1 sm:grid-cols-2 gap-4 mt-4">
-      {filteredData.map((match, index) => (
-        <div key={index} className="border p-4 rounded-lg shadow-lg bg-white text-center">
-          <h3 className="text-lg font-bold text-black">{match.ADVERSAIRE}</h3>
-          <p className="text-black">Saison : {match.SAISON} | Année : {match.ANNEE}</p>
-          <p className="text-black">Championnat : {match.CHAMPIONNAT}</p>
-          <p className="text-red-600 font-semibold">
-            {stat} : {match[stat]}
-          </p>
+  <li className="text-center flex-1 text-white">
+    <p className="text-xs font-semibold uppercase opacity-70 mb-1">RBD</p>
+    <p className="text-2xl font-bold tracking-wide">{data.reduce((total, match) => total + Number(match.RBD), 0)}</p>
+  </li>
+</ul>
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+      {/* Options de sélection et filtre */}
+      <div className="mb-4 mt-5 grid grid-cols-1 gap-4 sm:grid-cols-3 sm:gap-2 items-center">
+        <div className="flex flex-col">
+          <label htmlFor="stat-select" className="mb-1 text-sm">Sélectionnez une statistique :</label>
+          <select
+            id="stat-select"
+            value={stat}
+            onChange={(e) => setStat(e.target.value as keyof MatchData)}
+            className="border p-2 text-black rounded"
+          >
+            <option value="PTS">Points</option>
+            <option value="RBD">Rebonds</option>
+            <option value="AST">Assists</option>
+            <option value="STL">Steals</option>
+            <option value="BLK">Blocks</option>
+            <option value="EFF">Evaluation</option>
+            <option value="3P">3 Points</option>
+            <option value="1P">Lancer-Franc</option>
+            <option value="P">Paniers</option>
+            <option value="TO">Balles Perdues</option>
+          </select>
         </div>
-      ))}
+
+        <div className="flex flex-col">
+          <label htmlFor="num-results" className="mb-1 text-sm">Top :</label>
+          <input
+            id="num-results"
+            type="number"
+            value={numResults}
+            onChange={(e) => setNumResults(Number(e.target.value))}
+            className="border p-2 text-black rounded w-full"
+            min={1}
+          />
+        </div>
+
+        <div className="flex flex-col">
+          <label htmlFor="sort-order" className="mb-1 text-sm">Rang :</label>
+          <select
+            id="sort-order"
+            value={sortOrder}
+            onChange={(e) => setSortOrder(e.target.value as "top" | "bottom")}
+            className="border p-2 text-black rounded"
+          >
+            <option value="top">Meilleurs</option>
+            <option value="bottom">Pires</option>
+          </select>
+        </div>
+      </div>
+
+      <button
+        onClick={handleFilter}
+        className="w-full sm:w-auto bg-red-800 text-white p-2 rounded mt-2 sm:col-span-3"
+      >
+        Filtrer
+      </button>
+
+      {/* Affichage des cartes de match */}
+      <div className="grid grid-cols-1 sm:grid-cols-2 gap-4 mt-4">
+        {filteredData.map((match, index) => (
+          <div key={index} className="border p-4 rounded-lg shadow-lg bg-white text-center">
+            <h3 className="text-lg font-bold text-black">{match.ADVERSAIRE}</h3>
+            <p className="text-black">Saison : {match.SAISON} | Année : {match.ANNEE}</p>
+            <p className="text-black">Championnat : {match.CHAMPIONNAT}</p>
+            <p className="text-red-600 font-semibold">
+              {stat} : {match[stat]}
+            </p>
+          </div>
+        ))}
+      </div>
+
+      {filteredData.length === 0 && (
+        <p className="mt-6 text-gray-500 text-center">Aucun match trouvé pour cette statistique.</p>
+      )}
     </div>
-  
-    {/* Message si aucune donnée */}
-    {filteredData.length === 0 && (
-      <p className="mt-6 text-gray-500 text-center">Aucun match trouvé pour cette statistique.</p>
-    )}
-  </div>
-  
   );
 }
