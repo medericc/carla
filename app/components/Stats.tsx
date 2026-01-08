8c8834ba9b86e0946e3f59d3b33a2687c6e5e66f
medericc <bleublanctour1@>
2026-01-08 09:50:29 +0100

    ee

--- app/components/Stats.tsx
+++ app/components/Stats.tsx
@@ -28,11 +28,25 @@ import {
 
 const statsData = [
   { 
-    label: 'Premier Français Meilleur Marqueur d\'une franchise', 
+    label: 'Premier Français Meilleur Marqueur d\'une Franchise', 
     value: '06/05',
     icon: FaTrophy,
     color: 'from-yellow-500 to-amber-400',
     category: 'WNBA'
+  },
+  { 
+    label: 'Plus Jeune Français Topscorer d\'une Franchise', 
+    value: '21',
+    icon: FaTrophy,
+    color: 'from-yellow-500 to-amber-400',
+    category: 'Jeunesse'
+  },
+  { 
+    label: 'Plus Vieux Français Topscorer d\'une Franchise', 
+    value: '21',
+    icon: FaTrophy,
+    color: 'from-yellow-500 to-amber-400',
+    category: 'Vieillesse'
   },
   { 
     label: 'Meilleure Evaluation de l\'histoire des PO LFB', 
@@ -42,123 +56,166 @@ const statsData = [
     category: 'Playoffs'
   },
   { 
-    label: 'Meilleure Evaluation sur une campagne de PO LFB', 
+    label: 'Meilleure Evaluation sur une Campagne de PO LFB', 
     value: '21',
     icon: FaChartBar,
     color: 'from-teal-500 to-cyan-400',
     category: 'Playoffs'
   },
+ 
   { 
-    label: 'Meilleur marqueur jeune français en compétition internationale', 
+    label: 'Meilleur Marqueur Jeune Français en Compétition', 
     value: '18.4',
     icon: FaGlobeEurope,
     color: 'from-blue-500 to-indigo-400',
     category: 'International'
   },
   { 
-    label: 'Seul Français Meilleur Marqueur d\'une franchise', 
+    label: 'Seul Français Meilleur Marqueur d\'une Franchise', 
     value: '06/05',
     icon: FaUserNinja,
     color: 'from-purple-500 to-pink-400',
     category: 'Exclusivité'
   },
   { 
-    label: "Meilleure moyenne de points à 20 ans en LFB", 
+    label: "Meilleure Moyenne de Points à 20 ans en LFB", 
     value: "15",
     icon: FaStar,
     color: 'from-amber-500 to-orange-400',
     category: 'Jeunesse'
   },
   { 
-    label: "Plus jeune à atteindre 975 points en LFB", 
+    label: "Plus jeune à Atteindre 975 Points en LFB", 
     value: "20",
     icon: FaRocket,
     color: 'from-red-500 to-rose-400',
     category: 'Jeunesse'
   },
   { 
-    label: "Seule européenne meilleure scoreuse avec 40% à 3pts", 
+    label: "Seule Européenne Topscorer à 40% à 3pts", 
     value: "16.4",
     icon: FaBullseye,
     color: 'from-violet-500 to-purple-400',
     category: 'Précision'
   },
   { 
-    label: "Plus grand nombre de trophées de joueuse la plus décisive", 
+    label: "Joueuse la Plus Décisive de LFB", 
     value: "2",
     icon: FaAward,
     color: 'from-rose-500 to-pink-400',
     category: 'Distinctions'
   },
   { 
-    label: "Plus grand nombre de points par une européenne avec 40%+ à 3pts", 
+    label: "Européenne avec 40%+ à 3pts en LFB", 
     value: "16.4",
     icon: FaFire,
     color: 'from-orange-500 to-yellow-400',
     category: 'Précision'
   },
   { 
-    label: "Meilleure moyenne de points + passes à 20 ans en LFB", 
+    label: "Meilleure Moyenne Pts+Ast à 20 ans en LFB", 
     value: "20.3",
     icon: FaLightbulb,
     color: 'from-lime-500 to-green-400',
     category: 'Polyvalence'
   },
   { 
-    label: "Européenne la plus rapide à 975 points en LFB", 
+    label: "Européenne la Plus Rapide à 975 points en LFB", 
     value: "20",
     icon: FaRunning,
     color: 'from-cyan-500 to-blue-400',
     category: 'Rapidité'
   },
   { 
-    label: "Meilleure moyenne de points sur 3 saisons en LFB", 
+    label: "Meilleure Moyenne de Points sur 3 Saisons en LFB", 
     value: "15",
     icon: FaChartLine,
     color: 'from-emerald-600 to-teal-500',
     category: 'Consistance'
   },
   { 
-    label: "Meilleur total points + passes sur 3 saisons en LFB", 
+    label: "Meilleur Total Pts+Ast sur 3 Saisons en LFB", 
     value: "20.3",
     icon: FaBasketballBall,
     color: 'from-fuchsia-500 to-purple-400',
     category: 'Consistance'
   },
   { 
-    label: 'Meilleure PPG de l\'histoire des PO LFB', 
+    label: 'Meilleure PPG de l\'Histoire des PO LFB', 
     value: '20',
     icon: FaCrown,
     color: 'from-amber-600 to-yellow-500',
     category: 'Playoffs'
   },
   { 
-    label: 'Meilleure Marqueuse sur une campagne de PO LFB', 
+    label: 'Meilleure Marqueuse sur une Campagne de PO LFB', 
     value: '20',
     icon: FaMedal,
     color: 'from-red-600 to-orange-500',
     category: 'Playoffs'
   },
   { 
-    label: 'Plus Jeune Meilleure Marqueuse de l\'histoire de la LFB', 
+    label: 'Plus Vieille à Atteindre 20 ppg en PO LFB', 
+    value: '20',
+    icon: FaMedal,
+    color: 'from-red-600 to-orange-500',
+    category: 'Vieillesse'
+  },
+   { 
+    label: 'Plus Jeune à Atteindre 20 ppg en PO LFB', 
+    value: '20',
+    icon: FaMedal,
+    color: 'from-red-600 to-orange-500',
+    category: 'Jeunesse'
+  },
+   { 
+    label: 'Plus Vieille à Atteindre 20 epg en PO LFB', 
+    value: '20',
+    icon: FaMedal,
+    color: 'from-red-600 to-orange-500',
+    category: 'Vieillesse'
+  },
+   { 
+    label: 'Plus Jeune à Atteindre 20 epg en PO LFB', 
+    value: '20',
+    icon: FaMedal,
+    color: 'from-red-600 to-orange-500',
+    category: 'Jeunesse'
+  },
+  { 
+    label: 'Plus Jeune Meilleure Marqueuse de l\'Histoire de la LFB', 
     value: '20',
     icon: FaHistory,
     color: 'from-blue-600 to-cyan-500',
     category: 'Jeunesse'
   },
   { 
-    label: 'Première à éliminer l\'équipe MVP invaincue en quart', 
+    label: 'Première à Éliminer Bourges en Quart en MVP', 
     value: '26 pts',
     icon: FaFire,
     color: 'from-rose-600 to-pink-500',
     category: 'Exploits'
   },
   { 
-    label: 'Joueuse la plus décisive sur une campagne de PO LFB', 
+    label: 'Joueuse la Plus Décisive sur une Campagne de PO LFB', 
     value: '25.6',
     icon: FaAward,
     color: 'from-violet-600 to-purple-500',
     category: 'Playoffs'
+  },
+  { 
+    label: 'Plus Jeune Joueuse la plus décisive sur un PO LFB', 
+    value: '20',
+    icon: FaAward,
+    color: 'from-violet-600 to-purple-500',
+    category: 'Vieillesse'
+  },
+  { 
+    label: 'Plus Vieille Joueuse la plus décisive sur un PO LFB', 
+    value: '20',
+    icon: FaAward,
+    color: 'from-violet-600 to-purple-500',
+    category: 'Vieillesse'
   },
   { 
     label: 'PPM Record de l\'Eurobasket U20', 
@@ -168,21 +225,35 @@ const statsData = [
     category: 'International'
   },
   { 
-    label: 'Meilleure marqueuse française championne', 
+    label: 'Meilleure Marqueuse Française Championne', 
     value: '18.4',
     icon: FaFlag,
     color: 'from-yellow-600 to-amber-500',
     category: 'International'
   },
   { 
-    label: 'Seule européenne à finir une saison en 15-5 en LFB', 
+    label: 'Plus Jeune Française à Gagner avec 18.4 ppg (IT)', 
+    value: '18.4',
+    icon: FaFlag,
+    color: 'from-yellow-600 to-amber-500',
+    category: 'Jeunesse'
+  },
+  { 
+    label: 'Plus Vieille Française à Gagner avec 18.4 ppg (IT)', 
+    value: '18.4',
+    icon: FaFlag,
+    color: 'from-yellow-600 to-amber-500',
+    category: 'Vieillesse'
+  },
+  { 
+    label: 'Européenne à Finir en 15-5 en LFB', 
     value: '15-5',
     icon: FaUserNinja,
     color: 'from-indigo-600 to-blue-500',
     category: 'Exclusivité'
   },
   { 
-    label: 'Seule joueuse des Playoffs LFB à finir à +20 ppg', 
+    label: 'Joueuse des PO LFB à finir à +20 ppg', 
     value: '21',
     icon: FaHistory,
     color: 'from-teal-600 to-cyan-500',
@@ -196,92 +267,92 @@ const statsData = [
     category: 'Rapidité'
   },
   { 
-    label: 'Meilleure marqueuse française en finale aller', 
+    label: 'Meilleure Marqueuse Française en finale aller', 
     value: '25',
     icon: FaCrown,
     color: 'from-purple-600 to-pink-500',
     category: 'Finales'
   },
   { 
-    label: 'Plus jeune joueuse à scorer 176 pts en PO', 
+    label: 'Plus Jeune Joueuse à Scorer 176 pts en PO', 
     value: '20',
     icon: FaStar,
     color: 'from-rose-600 to-amber-500',
     category: 'Jeunesse'
   },
   { 
-    label: 'Plus jeune meilleure marqueuse des Eurocup PO', 
+    label: 'Plus Jeune Meilleure Marqueuse des Eurocup PO', 
     value: '20',
     icon: FaGlobeEurope,
     color: 'from-blue-600 to-indigo-500',
     category: 'International'
   },
   { 
-    label: 'Plus jeune joueuse la plus décisive des Eurocup PO', 
+    label: 'Plus Jeune à être Plus décisive des Eurocup PO', 
     value: '20',
     icon: FaAward,
     color: 'from-green-600 to-teal-500',
     category: 'International'
   },
   { 
-    label: 'Plus jeune joueuse la plus décisive de la LFB', 
+    label: 'Plus Jeune Joueuse la Plus Décisive de LFB', 
     value: '21.3',
     icon: FaChartLine,
     color: 'from-cyan-600 to-blue-500',
     category: 'Jeunesse'
   },
   { 
-    label: 'Première européenne à 228 Pts + Ast en PO', 
-    value: '228',
+    label: 'Européenne à 228 Pts + Ast en PO', 
+    value: '2025',
     icon: FaUserNinja,
     color: 'from-violet-600 to-purple-500',
     category: 'Exclusivité'
   },
   { 
-    label: 'Première joueuse à +160 pts à 55 % en Coupe d\'Europe', 
+    label: '+160 Pts à 55 % en Coupe d\'Europe', 
     value: '176',
     icon: FaBullseye,
     color: 'from-amber-600 to-yellow-500',
     category: 'Précision'
   },
   { 
-    label: 'Seule joueuse des PO LFB à 26 points à 90%', 
+    label: '26 points à 90% en PO LFB', 
     value: '26 pts',
     icon: FaLightbulb,
     color: 'from-emerald-600 to-green-500',
     category: 'Précision'
   },
   { 
-    label: 'Plus large victoire de l\'histoire de la Team France', 
+    label: 'Plus Large Victoire de l\'histoire de l\'EDF',
     value: '101',
     icon: FaUsers,
     color: 'from-red-600 to-rose-500',
     category: 'Équipe'
   },
- 
+
   { 
-    label: 'Draft WNBA la plus suivie de l\'histoire', 
+    label: 'Draft WNBA la Plus Suivie de l\'Histoire', 
     value: '2024',
     icon: FaTrophy,
     color: 'from-purple-600 to-pink-500',
     category: 'WNBA'
   },
   { 
-    label: 'Plus grosse vente d\'abonnements de la WNBA', 
+    label: 'Plus Grosse Vente d\'Abonnements de la WNBA', 
     value: '2025',
     icon: FaTicketAlt,
     color: 'from-green-600 to-emerald-500',
     category: 'WNBA'
   },
   { 
-    label: 'Plus grosse affluence pour un premier match WNBA', 
+    label: 'Affluence Pour un Premier Match WNBA', 
     value: '18,064',
     icon: FaUsers,
     color: 'from-orange-600 to-yellow-500',
     category: 'Affluence'
   },
   { 
-    label: 'Joueuse la plus rapide en First Team LFB', 
+    label: 'Joueuse la Plus Rapide en First Team LFB', 
     value: '2ème',
     icon: FaRocket,
     color: 'from-cyan-600 to-blue-500',
