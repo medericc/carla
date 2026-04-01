f6de5edb4861982418e13c1ced51ad5728b90698
medericc <bleublanctour1@>
2026-04-01 07:01:29 +0200

    ee

--- app/livestats/lucile/page.tsx
+++ app/livestats/lucile/page.tsx
@@ -46,6 +46,7 @@ export default function Home() {
     const matchLinksByPlayer: Record<string, { name: string; url: string }[]> = {
         "L. JEROME": [ 
             // { name: "Schio", url: "https://www.fiba.basketball/en/events/api/game-live-info/128317/detail" },
+      { name: "Quart 1", url: "https://fibalivestats.dcd.shared.geniussports.com/u/FFBB/2834908/bs.html" },
      
       { name: "ESBVA", url: "https://fibalivestats.dcd.shared.geniussports.com/u/FFBB/2724116/bs.html" },
      
