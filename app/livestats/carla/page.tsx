d73ae1b23b7b1186da55d9f501092db1b9cdec11
medericc <bleublanctour1@>
2026-05-10 03:37:39 +0200

    ee

--- app/livestats/carla/page.tsx
+++ app/livestats/carla/page.tsx
@@ -32,6 +32,7 @@ export default function Home() {
   const [isWaitingModalOpen, setIsWaitingModalOpen] = useState(false);
 
   const matchLinks = [
+    { name: "Chicago", url: "401856895" },
      { name: "L.A. Prepa", url: "401866521" },
  { name: "Seattle Prepa", url: "401866516" },
     
