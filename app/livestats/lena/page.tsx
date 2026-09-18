892c6ae07e13f7b7602a2554934c870e85535ac8
medericc <bleublanctour1@>
2026-09-19 00:33:26 +0200

    ee

--- app/livestats/lena/page.tsx
+++ app/livestats/lena/page.tsx
@@ -40,10 +40,10 @@ export default function Home() {
     const matchLinks: { name: string; url: string }[] = [  
  
  
-  //  { name: "Roche Vendée", url: "https://fibalivestats.dcd.shared.geniussports.com/u/FFBB/2879368/bs.html" },
+   { name: "Roche Vendée", url: "https://fibalivestats.dcd.shared.geniussports.com/u/FFBB/2879368/bs.html" },
      
  
-          { name: "Roche Vendée", url: "https://fibalivestats.dcd.shared.geniussports.com/u/FFBB/2713939/bs.html" },
+          // { name: "Monaco", url: "https://fibalivestats.dcd.shared.geniussports.com/u/FFBB/2713939/bs.html" },
         
       // { name: "Nice", url: "https://fibalivestats.dcd.shared.geniussports.com/u/FFBB/2713936/bs.html" },
         
@@ -170,7 +170,7 @@ export default function Home() {
 
     return (
         <div className="flex flex-col items-center justify-center min-h-screen p-6 sm:p-12 gap-8 bg-gray-100  text-gray-900 ">
-        <VideoHeader  src="/maelys.mp4" className="absolute top-0 left-0 w-full" />
+        <VideoHeader  src="/lena.webm" className="absolute top-0 left-0 w-full" />
         
         <main className="flex flex-col items-center gap-6 w-full max-w-lg sm:max-w-2xl md:max-w-4xl">
           <Select value={selectedLink} onValueChange={setSelectedLink}>
