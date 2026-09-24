afc8732387db4452e1663fd825809ebd5bd72ee6
medericc <bleublanctour1@>
2026-09-24 17:17:27 +0200

    ee

--- app/livestats/carla/page.tsx
+++ app/livestats/carla/page.tsx
@@ -35,25 +35,26 @@ export default function Home() {
      
     
 
-      { name: "Valkyries", url: "401857212" },
-       { name: "Sparks", url: "401857204" },
+      // { name: "Valkyries", url: "401857212" },
+      //  { name: "Sparks", url: "401857204" },
         { name: "Valkyries", url: "401857197" },
 //          { name: "Mercury", url: "401857193" },
 //           { name: "Valkyries", url: "401857188" },
 //            { name: "Dream", url: "401857180" },
-//             { name: "Dallas", url: "401857174" },
+            { name: "Dallas", url: "401857174" },
 //              { name: "Washington", url: "401857169" },
-//               { name: "Tempo", url: "401857163" },
+              { name: "Tempo", url: "401857163" },
 //                { name: "Mercury", url: "401857149" },
-//                 { name: "Seattle", url: "401857144" },
-//                  { name: "Lynx", url: "401857139" },
-//                   { name: "Seattle", url: "401857127" },
-//                    { name: "Toronto", url: "401857121" },
+                { name: "Seattle", url: "401857144" },
+                 { name: "Lynx", url: "401857139" },
+                  { name: "Seattle", url: "401857127" },
+                   { name: "Toronto", url: "401857121" },
 //                     { name: "LA", url: "401857108" },
 //                      { name: "Indiana", url: "401857104" },
-//                       { name: "LV", url: "401857095" },
-//                        { name: "Dallas", url: "401857091" },
-//                         { name: "Lynx", url: "401857078" },
+                      { name: "LV", url: "401857095" },
+                       { name: "Dallas", url: "401857091" },
+                   
+     { name: "Lynx", url: "401857078" },
     
     
     
@@ -62,26 +63,27 @@ export default function Home() {
     
     
 //     { name: "Washington", url: "401857071" },
-//      { name: "LV", url: "401857053" }, { name: "Dream", url: "401857059" }, { name: "Sun", url: "401857066" },
+     { name: "LV", url: "401857053" }, { name: "Dream", url: "401857059" }, 
+// { name: "Sun", url: "401857066" },
 //  { name: "Seattle", url: "401857040" },
     
      
 //      { name: "Washington", url: "401857031" },
     
 //       { name: "Chicago", url: "401857025" },
-    //      { name: "Chicago", url: "401857019" },
+         { name: "Chicago", url: "401857019" },
     //  { name: "Seattle", url: "401857001" },
     
-    //   { name: "Minnesota", url: "401856993" },
+      { name: "Minnesota", url: "401856993" },
     
-    //     { name: "Dallas", url: "401856988" },
+        { name: "Dallas", url: "401856988" },
     
     //    { name: "Las Vegas", url: "401856983" },
-    //   { name: "Los Angeles", url: "401856971" },
+      { name: "Los Angeles", url: "401856971" },
     
-    // { name: "Phoenix", url: "401856965" },
+    { name: "Phoenix", url: "401856965" },
     
-      // { name: "Valkyries", url: "401856957" },
+      { name: "Valkyries", url: "401856957" },
     
         // { name: "Indiana", url: "401856951" },
     
@@ -92,7 +94,7 @@ export default function Home() {
 //      { name: "Indiana", url: "401856921" },
 
 //     { name: "Connecticut", url: "401856919" },
-//     { name: "New York", url: "401856909" },
+    { name: "New York", url: "401856909" },
 
 
 //     { name: "New York", url: "401856903" },
